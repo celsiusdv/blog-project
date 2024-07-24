b8ad908ef3100e31b6221615b02ab8f7c1a3505d
celsiusdv <celsius.dev@protonmail.com>
2024-07-23 22:04:38 -0400

    added sample images

--- src/main/front-end/src/components/navbar/Login.tsx
+++ src/main/front-end/src/components/navbar/Login.tsx
@@ -11,7 +11,7 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
     const navigate = useNavigate();
     const authContext  = useContext<UserAuth | undefined>(AuthContext);//fill the data and manage it in AuthProvider.tsx
-    const [email,setEmail]=useState<string>("john@doe.com");
+    const [email,setEmail]=useState<string>("john@doe.com");//default admin input just for testing purposes
     const [password,setPassword]=useState<string>("hello");//default admin input just for testing purposes
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) =>{
